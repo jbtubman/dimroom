eec795c8218d446c0fcd37d4935dd9298afe1ae6
Jim Tubman <jim.tubman@me.com>
2025-07-02 15:22:26 -0600

    Added coordinates module and its tests.

---
+++ coordinates.hpp
@@ -0,0 +1,358 @@
+#pragma once
+
+#include <cmath>
+#include <cstdlib>
+#include <expected>
+#include <iomanip>
+#include <print>
+#include <regex>
+#include <sstream>
+#include <string>
+#include <string_view>
+#include <type_traits>
+
+// Geographical coordinates.
+// May be represented in the CSV file in two ways:
+// 36° 00' N, 138° 00' E
+// or
+// 51.05011, -114.08529
+// Positive latitudes are north.
+// Positive longitudes are east.
+
+namespace jt {
+// degrees/minutes regexps.
+const std::string deg_min_lat_s{R"(((\d{1,2})° (\d{2})' ([NS])))"};
+const std::regex deg_min_lat_rx{deg_min_lat_s};
+
+const std::string deg_min_long_s(R"(((\d{1,3})° (\d{2})' ([EW])))");
+const std::regex deg_min_long_rx{deg_min_long_s};
+
+// These constants give the index to the iterator in rti_vec that
+// contains the subexpression for that part of the degree/minute coordinate.
+// (They are magic numbers that were determined empirically for degree/minute
+// coordinates.)
+const int lat_deg{2};
+const int lat_min{3};
+const int lat_dir{4};
+
+const int long_deg{6};
+const int long_min{7};
+const int long_dir{8};
+
+const std::string deg_min_cooordinate_s("(\"" + deg_min_lat_s + ", " +
+                                        deg_min_long_s + "\")");
+const std::regex deg_min_cooordinate_rx{deg_min_cooordinate_s};
+
+// decimal coordinates regexps.
+
+const std::string decimal_lat_s{R"((-?\d{1,2}\.\d{5}))"};
+const std::regex decimal_lat_rx{decimal_lat_s};
+
+const std::string decimal_long_s{R"((-?\d{1,3}\.\d{5}))"};
+const std::regex decimal_long_rx{decimal_long_s};
+
+const std::string decimal_coordinate_s("(\"" + decimal_lat_s + ", " +
+                                       decimal_long_s + "\")");
+const std::regex decimal_coordinate_rx{decimal_coordinate_s};
+
+const std::string coordinate_s("^" + deg_min_cooordinate_s + "|" +
+                               decimal_coordinate_s + "$");
+const std::regex coordinate_rx{coordinate_s};
+
+// These constants give the index to the iterator in rti_vec that
+// contains the subexpression for that part of the decimal coordinate.
+// (They are magic numbers that were determined empirically for decimal
+// coordinates.)
+const int lat_decimal{1};
+const int long_decimal{2};
+
+bool is_deg_min_coordinate(const std::string& s) {
+    return std::regex_match(s, deg_min_cooordinate_rx);
+}
+
+bool is_decimal_coordinate(const std::string& s) {
+    return std::regex_match(s, decimal_coordinate_rx);
+}
+
+template <typename Invalid>
+std::expected<std::pair<float, float>, Invalid> parse_coordinate(
+    const std::string& coord, const std::regex rx);
+
+template <typename Invalid>
+std::expected<std::pair<float, float>, Invalid> parse_decimal_coordinate(
+    const std::string& dec_coord);
+
+template <typename Invalid>
+std::expected<std::pair<float, float>, Invalid> parse_deg_min_coordinate(
+    const std::string& deg_min_coord);
+
+class coordinate {
+   public:
+    enum class format { invalid, decimal, degrees_minutes };
+
+    format coordinate_format{format::invalid};
+    float latitude{0};
+    float longitude{0};
+
+    coordinate(format fmt, float lat_f, float long_f)
+        : coordinate_format{fmt}, latitude{lat_f}, longitude{long_f} {};
+
+    constexpr std::string_view coordinate_format_str() const {
+        using namespace std::literals::string_view_literals;
+        switch (coordinate_format) {
+            case format::invalid:
+                return "invalid"sv;
+
+            case format::decimal:
+                return "decimal"sv;
+
+            case format::degrees_minutes:
+                return "degrees_minutes"sv;
+        }
+    }
+
+    static coordinate make_coordinate(const std::string& s) {
+        auto coordinates = parse_decimal_coordinate<format>(s);
+        if (coordinates) {
+            float lat_f{std::get<0>(*coordinates)};
+            float long_f{std::get<1>(*coordinates)};
+            if (is_valid(lat_f, long_f)) {
+                return coordinate(format::decimal, lat_f, long_f);
+            }
+        }
+        // default-constructed coordinate is invalid.
+        return coordinate();
+    }
+
+    constexpr static bool is_valid(float lat_f, float long_f) {
+        if (lat_f > 90.0 || lat_f < -90.0) return false;
+        if (long_f > 180.0 || long_f < -180.0) return false;
+        return true;
+    }
+
+    constexpr bool is_valid() const {
+        return coordinate::is_valid(latitude, longitude);
+    }
+
+    // private:
+    coordinate() = default;
+    coordinate(const coordinate&) = default;
+    coordinate(coordinate&&) = default;
+    coordinate& operator=(const coordinate&) = default;
+    coordinate& operator=(coordinate&&) = default;
+};
+
+struct _coordinate_format_regex {
+    coordinate::format format;
+    const std::regex& rx;
+    _coordinate_format_regex(coordinate::format fmt, const std::regex& rgx)
+        : format(fmt), rx(rgx) {}
+};
+
+using coordinate_format_regex = _coordinate_format_regex;
+// std::pair<coordinate::format, const std::regex&>;
+
+coordinate_format_regex coordinate_type(const std::string& s) {
+    std::regex empty{};
+    auto fmt = is_deg_min_coordinate(s)
+                   ? coordinate::format::degrees_minutes
+                   : (is_decimal_coordinate(s) ? coordinate::format::decimal
+                                               : coordinate::format::invalid);
+    const auto rx =
+        is_deg_min_coordinate(s)
+            ? deg_min_cooordinate_rx
+            : (is_decimal_coordinate(s) ? decimal_coordinate_rx : empty);
+    return coordinate_format_regex{fmt, rx};
+}
+
+std::expected<std::pair<float, float>, coordinate::format>
+parse_decimal_coordinate(const std::string& coord) {
+    const auto mark_count = jt::decimal_coordinate_rx.mark_count();
+    std::vector<std::sregex_token_iterator> rti_vec;
+    ;
+    auto dec_end = std::sregex_token_iterator();
+    for (int subexpression_index = 1; subexpression_index <= mark_count;
+         subexpression_index++) {
+        auto it = std::sregex_token_iterator(coord.begin(), coord.end(),
+                                             jt::decimal_coordinate_rx,
+                                             subexpression_index);
+        if (it != dec_end) {
+            rti_vec.push_back(it);
+        }
+    }
+
+    if (rti_vec.empty()) {
+        return std::unexpected(coordinate::format::invalid);
+    }
+
+    auto lat_begin = rti_vec[lat_decimal];
+    auto long_begin = rti_vec[long_decimal];
+
+    if ((lat_begin != dec_end) && long_begin != dec_end) {
+        float lat_f = std::stof(lat_begin->str());
+        float long_f = std::stof(long_begin->str());
+        if (coordinate::is_valid(lat_f, long_f)) {
+            return std::pair{lat_f, long_f};
+        }
+    }
+
+    return std::unexpected(coordinate::format::invalid);
+}
+
+std::expected<std::pair<float, float>, coordinate::format>
+parse_deg_min_coordinate(const std::string& deg_min_coord) {
+    // create a vector of regex token iterators that point to the
+    // subexpressions found in the pattern matching.
+    std::vector<std::sregex_token_iterator> rti_vec;
+    for (int i = 1; i <= deg_min_cooordinate_rx.mark_count(); i++) {
+        auto dec_end = std::sregex_token_iterator();
+        auto it = std::sregex_token_iterator(deg_min_coord.begin(),
+                                             deg_min_coord.end(),
+                                             deg_min_cooordinate_rx, i);
+        if (it != dec_end) {
+            rti_vec.push_back(it);
+        }
+    }
+
+    if (rti_vec.empty()) {
+        return std::unexpected(coordinate::format::invalid);
+    }
+
+    const float lat_direction = rti_vec[lat_dir]->str() == "N" ? 1.0 : -1.0;
+    const std::string lat_min_s = rti_vec[lat_min]->str();
+    const float lat_min_f = std::stof(lat_min_s) / 60.0;
+    const std::string lat_deg_s = rti_vec[lat_deg]->str();
+    const float lat_f = (std::stof(lat_deg_s) + lat_min_f) * lat_direction;
+
+    const float long_direction = rti_vec[long_dir]->str() == "E" ? 1.0 : -1.0;
+    const std::string long_min_s = rti_vec[long_min]->str();
+    const float long_min_f = std::stof(long_min_s) / 60.0;
+    const std::string long_deg_s = rti_vec[long_deg]->str();
+    const float long_f = (std::stof(long_deg_s) + long_min_f) * long_direction;
+
+    return std::pair{lat_f, long_f};
+}
+
+std::expected<std::pair<float, float>, coordinate::format> parse_coordinate(
+    const std::string& coord) {
+    const coordinate_format_regex cfr = coordinate_type(coord);
+    const auto fmt = cfr.format;
+    switch (fmt) {
+        case coordinate::format::decimal:
+            return parse_decimal_coordinate(coord);
+
+        case coordinate::format::degrees_minutes:
+            return parse_deg_min_coordinate(coord);
+
+        default:
+            return std::unexpected(coordinate::format::invalid);
+    }
+}
+
+inline coordinate make_coordinate(const std::string& s) {
+    const coordinate_format_regex cfr = coordinate_type(s);
+    const coordinate::format fmt = cfr.format;
+
+    if (cfr.format == coordinate::format::decimal) {
+        auto coordinates = parse_decimal_coordinate(s);
+        if (coordinates) {
+            float lat_f{std::get<0>(*coordinates)};
+            float long_f{std::get<1>(*coordinates)};
+            if (coordinate::is_valid(lat_f, long_f)) {
+                return coordinate(coordinate::format::decimal, lat_f, long_f);
+            }
+        }
+    } else if (cfr.format == coordinate::format::degrees_minutes) {
+        auto coordinates = parse_deg_min_coordinate(s);
+        if (coordinates) {
+            float lat_f{std::get<0>(*coordinates)};
+            float long_f{std::get<1>(*coordinates)};
+            if (coordinate::is_valid(lat_f, long_f)) {
+                return coordinate(coordinate::format::degrees_minutes, lat_f,
+                                  long_f);
+            }
+        }
+    }
+
+    // default-constructed coordinate is invalid.
+    return coordinate();
+}
+
+};  // namespace jt
+
+// Formatter implementation based on the example found at:
+// https://www.en.cppreference.com/w/cpp/utility/format/formatter.html
+template <>
+struct std::formatter<jt::coordinate, char> {
+    template <class ParseContext>
+    constexpr ParseContext::iterator parse(ParseContext& ctx) {
+        auto it = ctx.begin();
+        if (it == ctx.end()) return it;
+        if (it != ctx.end() && *it != '}')
+            throw std::format_error("Invalid format args for coordinate.");
+
+        return it;
+    }
+
+    template <class FmtContext>
+    FmtContext::iterator format(jt::coordinate coord, FmtContext& ctx) const {
+        std::ostringstream out;
+
+        if (coord.coordinate_format == jt::coordinate::format::decimal) {
+            out << "\"";
+            const bool lat_negative = coord.latitude < 0;
+            out << std::setprecision(5);
+            out << std::fixed;
+            out << coord.latitude << ", ";
+
+            const bool long_negative = coord.longitude < 0;
+            out << coord.longitude;
+            out << "\"";
+        } else if (coord.coordinate_format ==
+                   jt::coordinate::format::degrees_minutes) {
+            out << "\"";
+            const float lat_f = coord.latitude;
+            const std::string direction = lat_f >= 0 ? "N" : "S";
+            const float trunc_lat_f = std::trunc(std::abs(lat_f));
+            //out << std::setprecision(2);
+            out << trunc_lat_f << "° ";
+            const float fraction_lat_f =
+                std::round((lat_f - trunc_lat_f) * 60.0f);
+            out << std::setw(2);
+            out << std::setfill('0');
+            out << fraction_lat_f << "' " << direction << ", ";
+
+            const float long_f = coord.longitude;
+            const std::string long_direction = long_f >= 0 ? "E" : "W";
+            const int trunc_long_f = std::truncf(std::abs(long_f));
+
+            out << trunc_long_f << "° ";
+            out << std::setw(2);
+            out << std::setfill('0');
+            const float fraction_long_f =
+                std::round((std::abs(long_f) - trunc_long_f) * 60.0f);
+            out << fraction_long_f << "' " << long_direction;
+            out << "\"";
+        } else {
+            out << "INVALID_COORDINATE";
+        }
+
+        // out << "coordinate{ ";
+        // out << "coordinate_format: \"" << coord.coordinate_format_str()
+        //     << "\", ";
+        // out << std::setprecision(8);
+        // if (coord.coordinate_format == jt::coordinate::format::decimal) {
+        //     out << "latitude: " << coord.latitude << ", ";
+        //     out << "longitude: " << coord.longitude << " }";
+        // } else if (coord.coordinate_format ==
+        //            jt::coordinate::format::degrees_minutes) {
+        //     out << "latitude: " << "TBD" << ", ";
+        //     out << "longitude: " << "TBD" << " }";
+        // } else {
+        //     out << "latitude: " << NAN << ", ";
+        //     out << "longitude: " << NAN << " }";
+        // }
+
+        return std::ranges::copy(std::move(out).str(), ctx.out()).out;
+    }
+};
---
+++ test/coordinates_test.cpp
@@ -0,0 +1,277 @@
+#include "../coordinates.hpp"
+
+#include <cmath>
+#include <concepts>
+#include <print>
+#include <string>
+#include <type_traits>
+
+#include "CppUnitTestFramework.hpp"
+
+namespace {
+using std::string;
+struct MyFixture {
+    //...
+
+    const float valid_decimal_lat{51.5};
+    const float valid_decimal_long{-114.0};
+    const string valid_decimal_coord{"\"51.50000, -114.00000\""};
+
+    const float valid_deg_min_lat{6.0};
+    const float valid_deg_min_long{-138.0};
+    const string valid_deg_min_coord{"\"6° 00' N, 138° 00' W\""};
+
+    const string invalid_decimal_coord{"\"51.05011 -114.08529\""};
+    const string invalid_deg_min_coord{"\"36° 00' K, 138° 00' E\""};
+};
+}  // namespace
+
+using namespace jt;
+using std::string;
+
+template <std::floating_point T, std::floating_point U>
+bool close(T l, U r) {
+    using TC = std::common_type_t<T, U, decltype(0.00001f)>;
+    const TC lc = l;
+    const TC rc = r;
+    const TC epsilon = 0.00001f;
+
+    return (std::abs(lc - rc) < epsilon);
+}
+
+TEST_CASE(MyFixture, ParseLatitude) {
+    SECTION("negative decimal") {
+        string lat{"-51.50000"};
+
+        auto match_begin =
+            std::sregex_iterator(lat.begin(), lat.end(), jt::decimal_lat_rx);
+        auto match_end = std::sregex_iterator();
+
+        CHECK_TRUE(match_begin != match_end);
+        CHECK_TRUE(match_begin->str() == lat);
+    }
+
+    SECTION("positive decimal") {
+        string lat{"51.50000"};
+
+        auto match_begin =
+            std::sregex_iterator(lat.begin(), lat.end(), jt::decimal_lat_rx);
+        auto match_end = std::sregex_iterator();
+
+        CHECK_TRUE(match_begin != match_end);
+        CHECK_TRUE(match_begin->str() == lat);
+    }
+
+    SECTION("bad input decimal") {
+        string lat{"51.5"};
+
+        auto match_begin =
+            std::sregex_iterator(lat.begin(), lat.end(), jt::decimal_lat_rx);
+        auto match_end = std::sregex_iterator();
+
+        CHECK_TRUE(match_begin == match_end);
+    }
+
+    SECTION("north dms") {
+        string lat{"36° 00' N"};
+
+        auto match_begin =
+            std::sregex_iterator(lat.begin(), lat.end(), jt::deg_min_lat_rx);
+        auto match_end = std::sregex_iterator();
+
+        CHECK_TRUE(match_begin != match_end);
+        CHECK_TRUE(match_begin->str() == lat);
+    }
+
+    SECTION("south dms") {
+        string lat{"3° 18' S"};
+
+        auto match_begin =
+            std::sregex_iterator(lat.begin(), lat.end(), jt::deg_min_lat_rx);
+        auto match_end = std::sregex_iterator();
+
+        CHECK_TRUE(match_begin != match_end);
+        CHECK_TRUE(match_begin->str() == lat);
+    }
+}
+
+TEST_CASE(MyFixture, ParseLongitude) {
+    SECTION("negative decimal") {
+        string lng{"-114.08529"};
+
+        auto match_begin =
+            std::sregex_iterator(lng.begin(), lng.end(), jt::decimal_long_rx);
+        auto match_end = std::sregex_iterator();
+
+        CHECK_TRUE(match_begin != match_end);
+        CHECK_TRUE(match_begin->str() == lng);
+    }
+
+    SECTION("positive decimal") {
+        string lng{"51.50000"};
+
+        auto match_begin =
+            std::sregex_iterator(lng.begin(), lng.end(), jt::decimal_long_rx);
+        auto match_end = std::sregex_iterator();
+
+        CHECK_TRUE(match_begin != match_end);
+        CHECK_TRUE(match_begin->str() == lng);
+    }
+
+    SECTION("bad input decimal") {
+        string lng{"51.5"};
+
+        auto match_begin =
+            std::sregex_iterator(lng.begin(), lng.end(), jt::decimal_long_rx);
+        auto match_end = std::sregex_iterator();
+
+        CHECK_TRUE(match_begin == match_end);
+    }
+
+    SECTION("east dms") {
+        string lng{"138° 00' E"};
+
+        auto match_begin =
+            std::sregex_iterator(lng.begin(), lng.end(), jt::deg_min_long_rx);
+        auto match_end = std::sregex_iterator();
+
+        CHECK_TRUE(match_begin != match_end);
+        CHECK_TRUE(match_begin->str() == lng);
+    }
+
+    SECTION("west dms") {
+        string lng{"3° 18' W"};
+
+        auto match_begin =
+            std::sregex_iterator(lng.begin(), lng.end(), jt::deg_min_long_rx);
+        auto match_end = std::sregex_iterator();
+
+        CHECK_TRUE(match_begin != match_end);
+        CHECK_TRUE(match_begin->str() == lng);
+    }
+
+    SECTION("bad input dms direction") {
+        string lng{"3° 18' K"};
+
+        auto match_begin =
+            std::sregex_iterator(lng.begin(), lng.end(), jt::decimal_long_rx);
+        auto match_end = std::sregex_iterator();
+
+        CHECK_TRUE(match_begin == match_end);
+    }
+
+    SECTION("bad input dms spacing") {
+        string lng{"3° 18'W"};
+
+        auto match_begin =
+            std::sregex_iterator(lng.begin(), lng.end(), jt::decimal_long_rx);
+        auto match_end = std::sregex_iterator();
+
+        CHECK_TRUE(match_begin == match_end);
+    }
+}
+
+TEST_CASE(MyFixture, Coordinate) {
+    SECTION("is decimal coordinates") {
+        CHECK_TRUE(jt::is_decimal_coordinate(MyFixture::valid_decimal_coord));
+        CHECK_TRUE(!jt::is_decimal_coordinate(MyFixture::valid_deg_min_coord));
+    }
+
+    SECTION("parse decimal coordinates") {
+        auto result =
+            jt::parse_decimal_coordinate(MyFixture::valid_decimal_coord);
+        CHECK_TRUE(result);
+
+        const auto lat = std::get<0>(*result);
+        CHECK_TRUE(close(lat, MyFixture::valid_decimal_lat));
+
+        const auto lng = std::get<1>(*result);
+        CHECK_TRUE(close(lng, MyFixture::valid_decimal_long));
+    }
+
+    SECTION("parse invalid decimal coordinates") {
+        auto result =
+            jt::parse_decimal_coordinate(MyFixture::invalid_decimal_coord);
+        CHECK_TRUE(!result);
+        CHECK_TRUE(result.error() == jt::coordinate::format::invalid);
+    }
+
+    SECTION("is degree minute coordinates") {
+        CHECK_TRUE(!jt::is_deg_min_coordinate(MyFixture::valid_decimal_coord));
+        CHECK_TRUE(jt::is_deg_min_coordinate(MyFixture::valid_deg_min_coord));
+    }
+
+    SECTION("parse deg min coordinates") {
+        auto result =
+            jt::parse_deg_min_coordinate(MyFixture::valid_deg_min_coord);
+        CHECK_TRUE(result);
+
+        const auto lat = std::get<0>(*result);
+        CHECK_TRUE(close(lat, MyFixture::valid_deg_min_lat));
+
+        const auto lng = std::get<1>(*result);
+        CHECK_TRUE(close(lng, MyFixture::valid_deg_min_long));
+    }
+
+    SECTION("decimal coordinates") {
+        string coord{"\"51.05011, -114.08529\""};
+
+        auto match_begin = std::sregex_iterator(coord.begin(), coord.end(),
+                                                jt::decimal_coordinate_rx);
+        auto match_end = std::sregex_iterator();
+
+        CHECK_TRUE(match_begin != match_end);
+        CHECK_TRUE(match_begin->str() == coord);
+    }
+
+    SECTION("decimal coordinates bad input") {
+        string coord{"\"51.05011, -114\""};
+
+        auto match_begin = std::sregex_iterator(coord.begin(), coord.end(),
+                                                jt::decimal_coordinate_rx);
+        auto match_end = std::sregex_iterator();
+
+        CHECK_TRUE(match_begin == match_end);
+    }
+    SECTION("dms coordinates") {
+        string coord{"\"36° 00' N, 138° 00' E\""};
+
+        auto match_begin = std::sregex_iterator(coord.begin(), coord.end(),
+                                                jt::deg_min_cooordinate_rx);
+        auto match_end = std::sregex_iterator();
+
+        CHECK_TRUE(match_begin != match_end);
+        CHECK_TRUE(match_begin->str() == coord);
+    }
+
+    SECTION("dms coordinates bad input") {
+        string coord{"\"36° 00' N, 138° 00' K\""};
+
+        auto match_begin = std::sregex_iterator(coord.begin(), coord.end(),
+                                                jt::deg_min_cooordinate_rx);
+        auto match_end = std::sregex_iterator();
+
+        CHECK_TRUE(match_begin == match_end);
+    }
+}
+
+TEST_CASE(MyFixture, Create) {
+    SECTION("valid decimal coordinates") {
+        auto coord = jt::make_coordinate(MyFixture::valid_decimal_coord);
+        CHECK_TRUE(coord.coordinate_format == jt::coordinate::format::decimal);
+        CHECK_TRUE(close(coord.latitude, MyFixture::valid_decimal_lat));
+        CHECK_TRUE(close(coord.longitude, MyFixture::valid_decimal_long));
+        std::println(stderr, "decimal coord: {}", coord);
+        coord.coordinate_format = jt::coordinate::format::degrees_minutes;
+        std::println(stderr, "deg/min coord: {}", coord);
+    }
+
+    SECTION("valid degree/minute coordinates") {
+        auto coord = jt::make_coordinate(MyFixture::valid_deg_min_coord);
+        CHECK_TRUE(coord.coordinate_format ==
+                   jt::coordinate::format::degrees_minutes);
+        CHECK_TRUE(close(coord.latitude, MyFixture::valid_deg_min_lat));
+        CHECK_TRUE(close(coord.longitude, MyFixture::valid_deg_min_long));
+        std::println(stderr, "deg/min coord: {}", coord);
+    }
+}
