f3ce5364ed5b803aabfaf9362aeed9af7336cd53
Jim Tubman <jim.tubman@me.com>
2025-07-02 21:00:17 -0600

    Code cleanup. Added documentation.

--- cell_types.hpp
+++ cell_types.hpp
@@ -1,8 +1,8 @@
 #pragma once
 
-#include <variant>
 #include <optional>
 #include <string>
+#include <variant>
 #include <vector>
 
 #include "coordinates.hpp"
@@ -10,7 +10,12 @@
 // Need to define what can be in a cell that is read in.
 
 namespace jt {
-  enum class value_type {
+
+using std::string;
+using std::vector;
+
+/// @brief Determines what kind of data is in the cells of a column.
+enum class value_type {
     undetermined,
     invalid,
     floating,
@@ -19,72 +24,78 @@ namespace jt {
     text,
     geo_coordinate,
     tags
-  };
+};
 
-  // Cannot create objects of this class.
-  struct invalid {
-    invalid() = delete;
-    invalid(const invalid&) = delete;
-    invalid(invalid&&) = delete;
-    invalid& operator=(invalid&) noexcept = delete;
-    invalid& operator=(invalid&&) noexcept = delete;
-    ~invalid() = delete;
-  };
+/// @brief Variant containing all the possible cell data value types.
+using _base_cell_types =
+    std::variant<float, bool, int, string, coordinate, vector<string> >;
 
-  using _base_cell_types = std::variant<
-    invalid,
-    float,
-    bool,
-    int,
-    std::string,
-    coordinate,
-    std::vector<std::string>
-  >;
+// If we know enough about a cell's contents to determine its type,
+// or to know that its contents do not match a valid type,
+// the optional has a value.
+// If the type is not known yet, there is no value.
 
-  // If we know enough about a cell's contents to determine its type,
-  // or to know that its contents do not match a valid type,
-  // the optional has a value.
-  // If the type is not known yet, there is no value.
-  using cell_type = std::optional<_base_cell_types>;
+/// @brief Cell data value type is optional because it might be determined later.
+using cell_type = std::optional<_base_cell_types>;
 
-  template <value_type V>
-  struct cell_value_type {};
+// This could be templated and made generic to all optional types
+// if I had more time.
 
-  template<>
-  struct cell_value_type<value_type::invalid> {
-    using type = invalid;
-  };
+/// @brief Compare two cell_type objects for equality.
+/// @param lhs
+/// @param rhs
+/// @return bool
+inline bool operator==(const cell_type& lhs, const cell_type& rhs) {
+    const bool lhs_has_value = lhs.has_value();
+    const bool rhs_has_value = rhs.has_value();
 
-  template<>
-  struct cell_value_type<value_type::floating> {
-    using type = float;
-  };
+    // both are empty; return true.
+    if (!(lhs_has_value || rhs_has_value)) return true;
 
-  template<>
-  struct cell_value_type<value_type::integer> {
-    using type = int;
-  };
+    // One has a value and the other doesn't.
+    if (lhs_has_value xor rhs_has_value) return false;
 
-  template<>
-  struct cell_value_type<value_type::boolean> {
-    using type = bool;
-  };
-
-  template<>
-  struct cell_value_type<value_type::text> {
-    using type = std::string;
-  };
-
-  template<>
-  struct cell_value_type<value_type::geo_coordinate> {
-    using type = coordinate;
-  };
-
-  template<>
-  struct cell_value_type<value_type::tags> {
-    using type = std::vector<std::string>;
-  };
-
-  template <value_type V>
-  using cell_value_type_t = cell_value_type<V>::type;
+    // Both have values; are they the same?
+    return (*lhs == *rhs);
 }
+
+/// @brief Finds the C++ type for a given cell data value type.
+/// @tparam V value_type
+template <value_type V>
+struct cell_value_type {};
+
+template <>
+struct cell_value_type<value_type::floating> {
+    using type = float;
+};
+
+template <>
+struct cell_value_type<value_type::integer> {
+    using type = int;
+};
+
+template <>
+struct cell_value_type<value_type::boolean> {
+    using type = bool;
+};
+
+template <>
+struct cell_value_type<value_type::text> {
+    using type = string;
+};
+
+template <>
+struct cell_value_type<value_type::geo_coordinate> {
+    using type = coordinate;
+};
+
+template <>
+struct cell_value_type<value_type::tags> {
+    using type = vector<string>;
+};
+
+/// @brief Gets the C++ data type for the value type.
+/// @tparam V value_type
+template <value_type V>
+using cell_value_type_t = cell_value_type<V>::type;
+}  // namespace jt
