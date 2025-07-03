0f427c49e9a9b45fd7d8b846fa1bf0af7e1e3181
Jim Tubman <jim.tubman@me.com>
2025-07-03 07:02:29 -0600

    Added row skeleton and test.

---
+++ row.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+// Rows of cells
+
+#include <vector>
+
+#include "cell.hpp"
+
+namespace jt {
+  using std::vector;
+
+  struct row {
+    // TODO: row struct.
+  };
+}
---
+++ test/row_test.cpp
@@ -0,0 +1,15 @@
+#include "../row.hpp"
+
+#include "CppUnitTestFramework.hpp"
+
+namespace {
+  struct MyFixture {
+    // TODO: row_test MyFixture.
+  };
+}
+
+using namespace jt;
+
+TEST_CASE(MyFixture, Row) {
+  // TODO: TEST_CASE Row.
+}
