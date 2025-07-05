26fb9cea5911fc63aaa4c6dab9ad0ae7c0fa8d37
Jim Tubman <jim.tubman@me.com>
2025-07-05 15:40:10 -0600

    Added tests for parser_utils and fixed bugs discovered by those tess.

--- parse_utils.hpp
+++ parse_utils.hpp
@@ -1,106 +1,219 @@
 #pragma once
 
+#include <algorithm>
+#include <functional>
+#include <print>
 #include <ranges>
 #include <regex>
 #include <string>
 #include <string_view>
 #include <vector>
+// turn on cassert.
+#if defined(NDEBUG)
+#define NDEBUG_WAS_DEFINED
+#undef NDEBUG
+#endif
+#include <cassert>
 
 #include "cell_types.hpp"
 #include "coordinates.hpp"
+#include "jt_concepts.hpp"
+#include "utility.hpp"
 
 namespace jt {
+using std::println;
 using std::regex;
 using std::string;
+using std::string_view;
 using std::vector;
-using std::operator""sv;
 namespace ranges = std::ranges;
 namespace views = std::ranges::views;
+using namespace std::string_literals;
 
-constexpr auto comma_substitute = "<<<COMMA>>>";
+using std::operator""sv;
+using std::operator""s;
 
+constexpr string comma_substitute{"<<<COMMA>>>"};
+constexpr string tags_regex_s{R"(""".*(,.*)*""")"};
+static const regex tags_regex(tags_regex_s);
+
+// Does a first pass at breaking a row on the commas.
+// Some field types have embedded commas so that must be fixed later.
 inline auto split_row(const string& row_s) {
-    using std::operator""sv;
-    auto res = row_s | views::split(","sv) | ranges::to<vector<string>>();
-    return res;
+    return ranges::split_view(row_s, ',') | ranges::to<vector<string>>();
 }
 
+// Coordinate fields have a comma in them, so they must be found and glued
+// together. This could probably be done better with std::adjacent_find and an
+// appropriate helper function. Then do a transform on the pair.
 inline string combine_coordinate_fields(const vector<string>& ss) {
-    string result("");
     bool in_coordinate = false;
     bool at_beginning = true;
-    for (auto s : ss) {
-        if (starts_with_coordinate(s)) {
-            in_coordinate = true;
-            if (!at_beginning) {
-                result.append(",");
-            }
-            result.append(s);
-        } else if (in_coordinate) {
-            result.append(comma_substitute);
-            result.append(s);
-            in_coordinate = false;
-        } else {
-            if (!at_beginning) {
-                result.append(",");
-            }
-            result.append(s);
-        }
 
-        at_beginning = false;
-    }
+    string result = ranges::fold_left(
+        ss, string(), [&at_beginning, &in_coordinate](string acc, string s) {
+            if (starts_with_coordinate(s)) {
+                in_coordinate = true;
+                if (!at_beginning) {
+                    acc.append(",");
+                }
+                acc.append(s);
+            } else if (in_coordinate) {
+                acc.append(comma_substitute);
+                acc.append(s);
+                in_coordinate = false;
+            } else {
+                if (!at_beginning) {
+                    acc.append(",");
+                }
+                acc.append(s);
+            }
+
+            at_beginning = false;
+            return acc;
+        });
 
     return result;
+}
+
+inline constexpr bool is_coordinate_pair(string_view lhs, string_view rhs) {
+    return (starts_with_coordinate({lhs.cbegin(), lhs.cend()}) &&
+            ends_with_coordinate({rhs.cbegin(), rhs.cend()}));
 }
 
 inline string combine_tag_fields(const vector<string>& ss) {
     string result("");
     bool in_tag = false;
     bool at_beginning = true;
+    println(stderr, "");
     for (auto s : ss) {
         if (s.starts_with(R"(""")")) {
+            assert(!in_tag);
+            println(stderr, "combine_tag_fields: entering tag with \"{}\"", s);
             in_tag = true;
             if (!at_beginning) {
+                println(stderr, "combine_tag_fields: appending comma");
                 result.append(",");
             }
+            println(stderr, "combine_tag_fields: appending \"{}\"", s);
             result.append(s);
         } else if (in_tag) {
-            result.append(comma_substitute);
-            result.append(s);
-            in_tag = s.ends_with(R"(""")");
-        } else {
+            println(stderr, "combine_tag_fields: inside tag with \"{}\"", s);
+            assert(!at_beginning);
             if (!at_beginning) {
+                println(stderr,
+                        "combine_tag_fields: appending comma substitute");
+                result.append(comma_substitute);
+            }
+            println(stderr, "combine_tag_fields: appending \"{}\"", s);
+            result.append(s);
+            if (s.ends_with(R"(""")")) {
+                println(stderr, "combine_tag_fields: exiting tag with \"{}\"",
+                        s);
+                in_tag = false;
+            }
+        } else {
+            println(stderr, "combine_tag_fields: outside tag with \"{}\"", s);
+            assert(!in_tag);
+            if ((!at_beginning) && (!in_tag)) {
+                println(stderr, "combine_tag_fields: appending comma");
                 result.append(",");
             }
+            println(stderr, "combine_tag_fields: appending \"{}\"", s);
             result.append(s);
         }
         at_beginning = false;
     }
 
+    println(stderr, "");
     return result;
 }
 
 inline vector<string> fix_quoted_fields(const string& row_s) {
     using std::operator""sv;
+    println(stderr, "\nrow_s: \"{}\"\n", row_s);
+
     // Split into strings with broken tags and coordinates;
     vector<string> broken_tags_coordinates_vec = split_row(row_s);
+    println(stderr, "\nbroken_tags_coordinates_vec: {}\n",
+            broken_tags_coordinates_vec);
+
     // Fix the coordinates and recombine into a string with broken tags.
     string broken_tags_s =
         combine_coordinate_fields(broken_tags_coordinates_vec);
+    println(stderr, "\nbroken_tags_s: \"{}\"\n", broken_tags_s);
+
     // Split into strings with broken tags.
     vector<string> broken_tags_vec = split_row(broken_tags_s);
+
     // Fix the tags and recombine into a string with <<<QUOTE>>> in it.
-    string quoted_string{combine_tag_fields(broken_tags_vec)};
+    string quoted_string = combine_tag_fields(broken_tags_vec);
+
     // Split the strings and replace each <<<QUOTE>>> with a comma.
     regex quote_rx{comma_substitute};
     auto split_string_vec =
         quoted_string | views::split(","sv) | ranges::to<vector<string>>();
     vector<string> result;
+
     for (auto qs : split_string_vec) {
         result.push_back(std::regex_replace(qs, quote_rx, ","));
     }
     return result;
 }
+
+// TODO: move determine_data_field_cell_type to the parser.
+
+// Given a string that contains text from a CSV data field, determine its data
+// type. If the string is empty, data type is cell_data_type::undetermined.
+inline e_cell_data_type determine_data_field_e_cell_data_type(
+    const string& cell_s) {
+    if (cell_s.empty()) return e_cell_data_type::undetermined;
+
+    if (cell_s == "Yes" or cell_s == "No") return e_cell_data_type::boolean;
+
+    if (is_decimal_coordinate(cell_s) || is_deg_min_coordinate(cell_s))
+        return e_cell_data_type::geo_coordinate;
+
+    e_cell_data_type vt = e_cell_data_type::undetermined;
+    try {
+        std::regex float_rx{R"(-?((\d+\.)|(\.\d+)|(\d+\.\d+)))"};
+        if (std::regex_match(cell_s, float_rx)) {
+            float f = std::stof(cell_s);
+            vt = e_cell_data_type::floating;
+        }
+    } catch (const std::exception& e) {
+#if DEBUG
+        std::println(stderr, "{}: {}", cell_s, e.what());
+#endif
+    }
+    if (vt == e_cell_data_type::floating) return vt;
+
+    vt = e_cell_data_type::undetermined;
+    try {
+        int i = std::stoi(cell_s);
+        vt = e_cell_data_type::integer;
+    } catch (const std::exception& e) {
+#if DEBUG
+        std::println(stderr, "{}: {}", cell_s, e.what());
+#endif
+    }
+    if (vt == e_cell_data_type::integer) return vt;
+
+    if (std::regex_match(cell_s, tags_regex)) return e_cell_data_type::tags;
+
+    // Otherwise, it is some kind of text string.
+    return e_cell_data_type::text;
+}
+
+/// @brief Type to hold intermediate parsing results.
+struct string_cvt_pos {
+    string str;
+    e_cell_data_type cvt;
+    std::size_t pos;
+};
+
+/// @brief Intermediate parsing results produced by zip functions.
+using string_cvt_pos_tuple = std::tuple<string, e_cell_data_type, std::size_t>;
 
 // Turn a CSV data line into a vector of tuples that represent
 // type, position, and value.
@@ -121,3 +234,7 @@ inline vector<string_cvt_pos_tuple> parse_row(const string& row_s) {
     return result;
 }
 }  // namespace jt
+#if defined(NDEBUG_WAS_DEFINED)
+#define NDEBUG
+#undef NDEBUG_WAS_DEFINED
+#endif
--- test/parse_utils_test.cpp
+++ test/parse_utils_test.cpp
@@ -14,7 +14,7 @@ using std::string;
 using std::vector;
 
 struct MyFixture : general_fixture {
-  // ...
+    // ...
 };
 }  // namespace
 
@@ -25,6 +25,7 @@ using std::println;
 using std::regex;
 using std::sregex_iterator;
 using namespace jt;
+using namespace std::string_literals;
 namespace ranges = std::ranges;
 namespace views = std::views;
 
@@ -32,38 +33,122 @@ namespace views = std::views;
 
 TEST_CASE(MyFixture, ParseUtils) {
     // TODO: proper test for "split row"
-    SECTION("split row") {
-        string row_s(MyFixture::sample_row_3);
-        auto result = split_row(row_s);
-        for (auto s : result) {
-            // println(stderr, "split row s: \"{}\"", s);
+    SECTION("split row 0") {
+        const string input{""};
+        const vector<string> expected({});
+        const vector<string> result = split_row(input);
+        CHECK_TRUE(result == expected);
+        if (result != expected) {
+            println(stderr, "\nsplit row 0");
+            println(stderr, "input:    \"{}\"", input);
+            println(stderr, "result:   \"{}\"", result);
+            println(stderr, "expected: \"{}\"\n", expected);
         }
     }
 
-    // TODO: proper test for "combine tag fields"
-    SECTION("combine tag fields") {
-        string row_s(MyFixture::sample_row_3);
-        auto ss = split_row(row_s);
-        string result = combine_tag_fields(ss);
-        println(stderr, "combine tag fields result: \"{}\"", result);
+    SECTION("split row 1") {
+        const string input{"1200"};
+        const vector<string> expected({"1200"s});
+        const vector<string> result = split_row(input);
+        CHECK_TRUE(result == expected);
+        if (result != expected) {
+            println(stderr, "\nsplit row 1");
+            println(stderr, "input:    \"{}\"", input);
+            println(stderr, "result:   \"{}\"", result);
+            println(stderr, "expected: \"{}\"\n", expected);
+        }
     }
 
-    // TODO: proper test for "combine coordinate fields"
-    SECTION("combine coordinate fields") {
-        string row_s(MyFixture::sample_row_3);
-        auto ss = split_row(row_s);
-        string result = combine_coordinate_fields(ss);
-        println(stderr, "combine coordinate fields result: \"{}\"", result);
+    SECTION("split row 2") {
+        const string input{"1200,Yes"};
+        const vector<string> expected({"1200"s, "Yes"s});
+        const vector<string> result = split_row(input);
+        CHECK_TRUE(result == expected);
+        if (result != expected) {
+            println(stderr, "\nsplit row 2");
+            println(stderr, "input:    \"{}\"", input);
+            println(stderr, "result:   \"{}\"", result);
+            println(stderr, "expected: \"{}\"\n", expected);
+        }
+    }
+
+    SECTION("split row 3") {
+        const string input{
+            R"(600,"36° 00' N, 138° 00' E",,Asia,,,,"""Mt Fuji, Fog""")"};
+        const vector<string> expected(
+            {"600"s, R"("36° 00' N)", R"( 138° 00' E")", ""s, "Asia"s, ""s, ""s,
+             ""s, R"("""Mt Fuji)", R"( Fog""")"});
+        const vector<string> result = split_row(input);
+        CHECK_TRUE(result == expected);
+        if (result != expected) {
+            println(stderr, "\nsplit row 3");
+            println(stderr, "input:    \"{}\"", input);
+            println(stderr, "result:   \"{}\"", result);
+            println(stderr, "expected: \"{}\"\n", expected);
+        }
+    }
+
+    SECTION("combine decimal coordinate fields") {
+        const string input_s{R"(1200,"51.05011, -114.08529",Yes)"};
+        const vector<string> input = split_row(input_s);
+        const string expected{"1200"s + ","s + MyFixture::dec_cord_start +
+                              comma_substitute + MyFixture::dec_cord_end +
+                              ","s + "Yes"s};
+        auto result = combine_coordinate_fields(input);
+        CHECK_TRUE(result == expected);
+        if (result != expected) {
+            println(stderr, "\ninput_s:  \"{}\"", input_s);
+            println(stderr, "input:    {}", input);
+            println(stderr, "result:   \"{}\"", result);
+            println(stderr, "expected: \"{}\"\n", expected);
+        }
+    }
+
+    SECTION("combine dms coordinate fields") {
+        const string input_s{R"(600,"36° 00' N, 138° 00' E",,Asia)"};
+        const vector<string> input = split_row(input_s);
+        const string expected = "600"s + ","s + MyFixture::dms_cord_start +
+                                comma_substitute + MyFixture::dms_cord_end +
+                                ","s + ""s + ","s + "Asia"s;
+        const string result = combine_coordinate_fields(input);
+        CHECK_TRUE(result == expected);
+        if (result != expected) {
+            println(stderr, "\ninput_s:  \"{}\"", input_s);
+            println(stderr, "input:    {}", input);
+            println(stderr, "result:   \"{}\"", result);
+            println(stderr, "expected: \"{}\"\n", expected);
+        }
+    }
+
+    SECTION("combine tag fields") {
+        const vector<string> input{"Yes"s, ""s,          "32",
+                                   "Y",    "Flames",     R"("""Urban)",
+                                   " B&W",  R"( Dusk""")"};
+        const string expected = R"(Yes,,32,Y,Flames,"""Urban)" +
+                                comma_substitute + " B&W" + comma_substitute +
+                                R"( Dusk""")";
+        const string result = combine_tag_fields(input);
+        CHECK_TRUE(result == expected);
+        if (result != expected) {
+            println(stderr, "\ncombine tag fields");
+            println(stderr, "input:    \"{}\"", input);
+            println(stderr, "result:   \"{}\"", result);
+            println(stderr, "expected: \"{}\"\n", expected);
+        }
     }
 
     SECTION("fix quoted fields") {
         string row_s(MyFixture::sample_row_3);
         auto result = fix_quoted_fields(row_s);
         int i = 0;
-        for (auto s : result) {
-            println(stderr, "fix quoted fields result[{}]: \"{}\"", i++, s);
-        }
+        // for (auto s : result) {
+        //     println(stderr, "fix quoted fields result[{}]: \"{}\"", i++, s);
+        // }
         CHECK_TRUE(valid_decimal_coord == result[6]);
+        if (valid_decimal_coord != result[6]) {
+            println(stderr, "valid_decimal_coord: \"{}\"", valid_decimal_coord);
+            println(stderr, "result[6]:           \"{}\"", result[6]);
+        }
         CHECK_TRUE(valid_tag_sample_row_3 == result[12]);
     }
 }
