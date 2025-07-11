389cfa3015dd2de905a4b9c36c3da335f4733d47
Jim Tubman <jim.tubman@me.com>
2025-07-11 10:29:11 -0600

    Worked on getting values out of the cells.

--- cell.hpp
+++ cell.hpp
@@ -12,6 +12,7 @@
 #include <string>
 #include <string_view>
 #include <type_traits>
+#include <variant>
 #include <vector>
 
 #include "cell_types.hpp"
@@ -22,7 +23,10 @@
 namespace jt {
 using std::regex;
 using std::string;
+using std::swap;
 using std::vector;
+namespace ranges = std::ranges;
+namespace views = std::ranges::views;
 
 // The parser::header_and_data class is our starting point.
 // Within that are the all_data_fields and data_fields types.
@@ -33,6 +37,12 @@ class data_cell {
     // std::size_t position{};
     e_cell_data_type data_type{e_cell_data_type::undetermined};
     cell_value_type value{};
+
+    data_cell(const parser::data_field& df)
+        : data_type{df.data_type},
+          value{make_cell_value_type(df.text, df.data_type)} {
+        const e_cell_data_type df_e_type = df.data_type;
+    }
 
     data_cell(e_cell_data_type ecdt, const cell_value_type& cdt)
         : data_type{ecdt}, value{cdt} {}
@@ -48,7 +58,6 @@ class data_cell {
           value{std::move(other.value)} {}
 
     data_cell& operator=(const data_cell& other) {
-        using std::swap;
         data_cell tmp{other};
         swap(data_type, tmp.data_type);
         swap(value, tmp.value);
@@ -56,7 +65,6 @@ class data_cell {
     }
 
     data_cell& operator=(data_cell&& other) {
-        using std::swap;
         data_cell tmp{std::move(other)};
         swap(data_type, tmp.data_type);
         swap(value, tmp.value);
@@ -70,53 +78,109 @@ class data_cell {
         return value.has_value();
     }
 
+    float get_float() const { return get<float>(*value); }
+
+    bool get_bool() const { return get<bool>(*value); }
+
+    int get_int() const { return get<int>(*value); }
+
+    string get_string() const { return get<string>(*value); }
+
+    coordinate get_coordinate() const { return get<coordinate>(*value); }
+
+    vector<string> get_tags() const { return get<vector<string>>(*value); }
+
+#pragma region _getvalue
     template <e_cell_data_type E>
     static auto _get_value(const data_cell& dc, Construct<E>);
 
     static auto _get_value(const data_cell& dc,
-                          Construct<e_cell_data_type::boolean>) -> bool {
+                           Construct<e_cell_data_type::boolean>) -> bool {
         return std::get<bool>(*(dc.value));
     }
 
     static auto _get_value(const data_cell& dc,
-                          Construct<e_cell_data_type::floating>) -> float {
+                           Construct<e_cell_data_type::floating>) -> float {
         return std::get<float>(*(dc.value));
     }
 
     static auto _get_value(const data_cell& dc,
-                          Construct<e_cell_data_type::geo_coordinate>)
+                           Construct<e_cell_data_type::geo_coordinate>)
         -> coordinate {
         return std::get<coordinate>(*(dc.value));
     }
 
     static auto _get_value(const data_cell& dc,
-                          Construct<e_cell_data_type::integer>) -> int {
+                           Construct<e_cell_data_type::integer>) -> int {
         return std::get<int>(*(dc.value));
     }
 
     static auto _get_value(const data_cell& dc,
-                          Construct<e_cell_data_type::tags>) -> vector<string> {
+                           Construct<e_cell_data_type::tags>)
+        -> vector<string> {
         return std::get<vector<string>>(*(dc.value));
     }
 
     static auto _get_value(const data_cell& dc,
-                          Construct<e_cell_data_type::text>) -> string {
+                           Construct<e_cell_data_type::text>) -> string {
         return std::get<string>(*(dc.value));
     }
 
     static auto _get_value(const data_cell& dc,
-                          Construct<e_cell_data_type::undetermined>) -> void {
+                           Construct<e_cell_data_type::undetermined>) -> void {
         // nothing.
     }
-
-    // auto get_value() {
-    //     constexpr e_cell_data_type dtype{data_type};
-    //     using zz = std::add_const<dtype>;
-    //     using dtype = Construct<zz>;
-    //     return _get_value(*this, Construct);
-    // }
+#pragma endregion _get_value
 
     constexpr bool has_value() const noexcept { return value.has_value(); }
+
+    template <typename ECellDataTagType>
+    auto get_value(ECellDataTagType tag) {};
+
+    template <>
+    auto get_value<undetermined_tag>(undetermined_tag) {
+        return std::nullopt;
+    }
+
+    template <>
+    auto get_value<invalid_tag>(invalid_tag) {
+        return std::nullopt;
+    }
+
+    template <>
+    auto get_value<floating_tag>(floating_tag) {
+        return std::get<float>(*(this->value));
+    }
+
+    template <>
+    auto get_value<boolean_tag>(boolean_tag) {
+        return std::get<bool>(*(this->value));
+    }
+
+    template <>
+    auto get_value<integer_tag>(integer_tag) {
+        return std::get<int>(*(this->value));
+    }
+
+    template <>
+    auto get_value<text_tag>(text_tag) {
+        return std::get<string>(*(this->value));
+    }
+
+    template <>
+    auto get_value<geo_coordinate_tag>(geo_coordinate_tag) {
+        return std::get<coordinate>(*(this->value));
+    }
+
+    template <>
+    auto get_value<tags_tag>(tags_tag) {
+        return std::get<vector<string>>(*(this->value));
+    }
+
+    template <>
+    auto get_value<sentinel_tag>(sentinel_tag) {
+        return std::nullopt;
+    }
 
     static cell_value_type make_cell_value_type(
         const string& s, /* std::size_t pos, */
@@ -159,19 +223,62 @@ class data_cell {
                 auto len = s.size();
                 string trimmed_string(s.begin() + 3, s.begin() + len - 3);
                 auto tags = trimmed_string | std::views::split(", "sv) |
-                            std::ranges::to<vector<string>>();
+                            ranges::to<vector<string>>();
                 cell_value_types bct = tags;
                 return cell_value_type{bct};
             }
+
+            case e_cell_data_type::SENTINEL:
+                return cell_value_type{};
+
+            default:
+                return cell_value_type{};
         }
+    }
+
+    static cell_value_type make_cell_value_type(const parser::data_field& df) {
+        return make_cell_value_type(df.text, df.data_type);
     }
 
     static data_cell make_data_cell(const parser::data_field& df) {
         return data_cell{df.data_type, make_cell_value_type(df)};
     }
 
-    static cell_value_type make_cell_value_type(const parser::data_field& df) {
-        return make_cell_value_type(df.text, df.data_type);
+    static data_cell make_data_cell(parser::data_field&& df) {
+        const parser::data_field df_{std::move(df)};
+        return data_cell{df_.data_type, make_cell_value_type(df_)};
+    }
+
+    static vector<data_cell> make_data_cells(
+        const vector<parser::data_field>& dfs) {
+        return dfs | std::views::transform([](auto&& df) {
+                   return make_data_cell(std::move(df));
+               }) |
+               ranges::to<vector<data_cell>>();
+    }
+
+    static vector<data_cell> make_data_cells(vector<parser::data_field>&& dfs) {
+        const vector<parser::data_field> dfs_{std::move(dfs)};
+        return dfs_ | views::transform([](auto&& df) {
+                   return make_data_cell(std::move(df));
+               }) |
+               ranges::to<vector<data_cell>>();
+    }
+
+    static vector<vector<data_cell>> make_all_data_cells(
+        const vector<vector<parser::data_field>>& dfs_vec) {
+        return dfs_vec | views::transform([](auto&& dfs) {
+                   return make_data_cells(std::move(dfs));
+               }) |
+               ranges::to<vector<vector<data_cell>>>();
+    }
+
+    static vector<vector<data_cell>> make_all_data_cells(
+        vector<vector<parser::data_field>>&& dfs_vec) {
+        return std::move(dfs_vec) | views::transform([](auto&& dfs) {
+                   return make_data_cells(std::move(dfs));
+               }) |
+               ranges::to<vector<vector<data_cell>>>();
     }
 
     template <e_cell_data_type V>
@@ -214,8 +321,8 @@ class data_cell {
 
         auto len = s.size();
         string trimmed_string(s.begin() + 3, s.begin() + len - 3);
-        auto tags = trimmed_string | std::views::split(", "sv) |
-                    std::ranges::to<vector<string>>();
+        auto tags = trimmed_string | views::split(", "sv) |
+                    ranges::to<vector<string>>();
         cell_value_types bct = tags;
         return cell_value_type{bct};
     }
--- test/cell_test.cpp
+++ test/cell_test.cpp
@@ -7,13 +7,14 @@
 #include <string_view>
 #include <vector>
 
-#include "CppUnitTestFramework.hpp"
 #include "../cell_types.hpp"
-#include "general_fixture.hpp"
 #include "../parser.hpp"
+#include "CppUnitTestFramework.hpp"
+#include "general_fixture.hpp"
 
 namespace {
 using std::string;
+using std::vector;
 using namespace jt;
 
 struct MyFixture : general_fixture {
@@ -24,11 +25,25 @@ struct MyFixture : general_fixture {
     const data_field sample_text_df{"Iceland.png", ecdt::text};
     const data_field sample_float_df{"8.35", ecdt::floating};
     const data_field sample_integer_df{"800", ecdt::integer};
-    const data_field sample_dec_coord_df{R"("51.05011, -114.08529")", ecdt::geo_coordinate};
-    const data_field sample_dms_coord_df{R"("36° 00' N, 138° 00' E")", ecdt::geo_coordinate};
+    const data_field sample_dec_coord_df{R"("51.05011, -114.08529")",
+                                         ecdt::geo_coordinate};
+    const data_field sample_dms_coord_df{R"("36° 00' N, 138° 00' E")",
+                                         ecdt::geo_coordinate};
     const data_field sample_false_boolean_df{"No", ecdt::boolean};
     const data_field sample_true_boolean_df{"Yes", ecdt::boolean};
-    const data_field sample_tag_df{R"("""Johnson, Volcano, Dusk""")", ecdt::tags};
+    const data_field sample_tag_df{R"("""Johnson, Volcano, Dusk""")",
+                                   ecdt::tags};
+    const data_field sample_invalid_df{"INVALID", ecdt::invalid};
+    const data_field sample_undeterminated_df{"UNDETERMINED",
+                                              ecdt::undetermined};
+
+    const vector<data_field> value_dfs{
+        sample_text_df,         sample_float_df,     sample_integer_df,
+        sample_dec_coord_df,    sample_dms_coord_df, sample_false_boolean_df,
+        sample_true_boolean_df, sample_tag_df};
+
+    const vector<data_field> empty_dfs{sample_invalid_df,
+                                       sample_undeterminated_df};
 };
 }  // namespace
 
@@ -57,7 +72,6 @@ auto find_final(Collection&& collection) {
     return current;
 }
 
-
 TEST_CASE(MyFixture, MakeCellTypes) {
     SECTION("make_cell_value_type text") {
         const auto& input = MyFixture::sample_text_df;
@@ -83,29 +97,23 @@ TEST_CASE(MyFixture, MakeCellTypes) {
         const int result_i = std::get<int>(*result);
         CHECK_TRUE(result_i == 800);
     }
-    // const data_field sample_dec_coord_df{R"("51.05011, -114.08529")", ecdt::geo_coordinate};
+    // const data_field sample_dec_coord_df{R"("51.05011, -114.08529")",
+    // ecdt::geo_coordinate};
     SECTION("make_cell_value_type decimal geo_coordinate") {
         const auto& input = MyFixture::sample_dec_coord_df;
         const auto result = data_cell::make_cell_value_type(input);
         CHECK_TRUE(result);
     }
-    // const data_field sample_dms_coord_df{R"("36° 00' N, 138° 00' E")", ecdt::geo_coordinate};
-    SECTION("make_cell_value_type dms geo_coordinate") {
-
-    }
+    // const data_field sample_dms_coord_df{R"("36° 00' N, 138° 00' E")",
+    // ecdt::geo_coordinate};
+    SECTION("make_cell_value_type dms geo_coordinate") {}
     // const data_field sample_boolean_df{"No", ecdt::boolean};
-    SECTION("make_cell_value_type false boolean") {
-
-    }
+    SECTION("make_cell_value_type false boolean") {}
     // const data_field sample_boolean_df{"Yes", ecdt::boolean};
-    SECTION("make_cell_value_type true boolean") {
-
-    }
-    // const data_field sample_tag_df{R"("""Johnson, Volcano, Dusk""")", ecdt::tags};
-    SECTION("make_cell_value_type tags") {
-
-    }
-
+    SECTION("make_cell_value_type true boolean") {}
+    // const data_field sample_tag_df{R"("""Johnson, Volcano, Dusk""")",
+    // ecdt::tags};
+    SECTION("make_cell_value_type tags") {}
 }
 
 TEST_CASE(MyFixture, Cell) {
@@ -164,5 +172,30 @@ TEST_CASE(MyFixture, Cell) {
                 println(stderr, "res: {}", res);
             }
         }
+    }
+
+    SECTION("converting 1") {
+        const parser::header_and_data input = parse_lines(MyFixture::sample_csv_rows);
+        const auto all_data_cells = data_cell::make_all_data_cells(input.get_data_fields());
+        const auto first_row_data_cells =
+            data_cell::make_data_cells(input.get_data_fields()[0]);
+        const auto first_result = first_row_data_cells[0];
+        CHECK_TRUE(first_result.value);
+        const auto val = *first_result.value;
+        const auto s = std::get<string>(val);
+        CHECK_TRUE(s == "Iceland.png");
+    }
+
+    SECTION("converting Japan") {
+        const parser::header_and_data input = parse_lines(MyFixture::sample_csv_rows);
+        const auto all_data_cells = data_cell::make_all_data_cells(input.get_data_fields());
+        // Japan is the third row.
+        const auto cells = all_data_cells[2];
+        // R"(Filename,Type,Image Size (MB),Image X,Image Y,DPI,(Center) Coordinate,Favorite,Continent,Bit color,Alpha,Hockey Team,User Tags)"
+        // R"(Japan.jpeg,jpeg,26.4,600,800,600,"36° 00' N, 138° 00' E",,Asia,,,,"""Mt Fuji, Fog""")",
+        const auto filename = cells[0];
+        CHECK_TRUE(filename);
+        CHECK_TRUE(true);
+        CHECK_TRUE(filename.get_string() == "Japan.jpeg");
     }
 }
