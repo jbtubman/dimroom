60607be9699edf7c08081d4709d00cdea781ecd1
Jim Tubman <jim.tubman@me.com>
2025-07-12 05:58:23 -0600

    Began filling in the table class.

--- table.hpp
+++ table.hpp
@@ -2,16 +2,130 @@
 
 // holds the column and row information.
 
+#include <algorithm>
+#include <map>
+#include <ranges>
+#include <set>
+#include <string>
+#include <utility>
 #include <vector>
 
-#include "column.hpp"
 #include "cell.hpp"
+#include "column.hpp"
+#include "parser.hpp"
 #include "row.hpp"
+#include "utility.hpp"
 
 namespace jt {
-  using std::vector;
+using std::map;
+using std::pair;
+using std::set;
+using std::string;
+using std::vector;
+namespace ranges = std::ranges;
+namespace views = std::ranges::views;
 
-  struct table {
-    // TODO: table.
-  };
-}
+const bool has_enumerate_view =
+#if defined(__cpp_lib_ranges_enumerate)
+    true;
+#else
+    false;
+#endif
+
+struct table {
+    // RESUME HERE: table.
+
+    /// @brief The names of the header fields and their e_cell_data_type values.
+    parser::header_fields header_fields_{};
+
+    using cell_rows = vector<data_cells>;
+
+    /// @brief vector of rows of cells.
+    cell_rows cell_rows_{};
+
+    using column_name_index_map_t = map<string, size_t>;
+
+    /// @brief Maps a column name to its index in the vector of headers.
+    column_name_index_map_t column_name_index_map{};
+
+    using type_column_name_set = set<string>;
+
+    type_column_name_set undetermined_fields{};
+    type_column_name_set invalid_fields{};
+    type_column_name_set floating_fields{};
+    type_column_name_set boolean_fields{};
+    type_column_name_set integer_fields{};
+    type_column_name_set text_fields{};
+    type_column_name_set geo_coordinate_fields{};
+    type_column_name_set tags_fields{};
+
+    vector<type_column_name_set> all_type_column_name_sets() {
+        return {undetermined_fields,   invalid_fields, floating_fields,
+                boolean_fields,        integer_fields, text_fields,
+                geo_coordinate_fields, tags_fields};
+    }
+
+    using type_column_name_map_t = map<e_cell_data_type, type_column_name_set>;
+
+    /// @brief Associates a e_cell_data_type value with its corresponding
+    /// type_column_name_set.
+    type_column_name_map_t type_to_column_name_map{};
+
+    /// @brief Creates a map of column names to header index values.
+    /// @param hfs
+    /// @return
+    constexpr static column_name_index_map_t headers_to_column_name_index_map(
+        parser::header_fields hfs) {
+        auto hf_names = hfs | views::transform([](parser::header_field hf) {
+                            return hf.name;
+                        });
+        return views::zip(hf_names, infinite_ints_vw()) |
+               ranges::to<column_name_index_map_t>();
+    }
+
+    table() {};
+
+    table(const parser::header_fields& hfs, const cell_rows& dcs)
+        : header_fields_{hfs},
+          cell_rows_{dcs},
+          column_name_index_map{
+              headers_to_column_name_index_map(header_fields_)} {
+        type_to_column_name_map =
+            ranges::zip_view(all_e_cell_data_types(),
+                             all_type_column_name_sets()) |
+            ranges::to<type_column_name_map_t>();
+    }
+
+    table(const table& other)
+        : header_fields_{other.header_fields_},
+          cell_rows_{other.cell_rows_},
+          column_name_index_map{other.column_name_index_map},
+          type_to_column_name_map{other.type_to_column_name_map} {}
+
+    table(table&& other)
+        : header_fields_{std::move(other.header_fields_)},
+          cell_rows_{std::move(other.cell_rows_)},
+          column_name_index_map{std::move(other.column_name_index_map)},
+          type_to_column_name_map{std::move(other.type_to_column_name_map)} {}
+
+    void swap(table& other) {
+        using std::swap;
+        swap(header_fields_, other.header_fields_);
+        swap(cell_rows_, other.cell_rows_);
+        swap(column_name_index_map, other.column_name_index_map);
+        swap(type_to_column_name_map, other.type_to_column_name_map);
+    }
+
+    table& operator=(const table& other) {
+        table tmp{other};
+        swap(tmp);
+        return *this;
+    }
+
+    table& operator=(table&& other) {
+        table tmp{std::move(other)};
+        swap(tmp);
+        return *this;
+    }
+};
+}  // namespace jt
--- test/table_test.cpp
+++ test/table_test.cpp
@@ -1,18 +1,51 @@
 #include "../table.hpp"
 
-#include "CppUnitTestFramework.hpp"
+#include <algorithm>
+#include <print>
+#include <string>
+#include <vector>
 
+#include "../cell_formatter.hpp"
+#include "../parser.hpp"
+#include "../parser_formatter.hpp"
+#include "CppUnitTestFramework.hpp"
 #include "general_fixture.hpp"
 
 namespace {
-  struct MyFixture : general_fixture {
+struct MyFixture : general_fixture {
     // ...
-  };
-}
+};
+}  // namespace
 
+using std::println;
 using namespace jt;
+using namespace std::string_literals;
+using std::operator""s;
 
 // TODO: proper tests for teable_test.cpp
 TEST_CASE(MyFixture, Table) {
-  // TODO: TEST_CASE Table.
+    // TODO: TEST_CASE Table.
+    SECTION("test") {
+        const parser::header_and_data input =
+            parse_lines(MyFixture::sample_csv_rows);
+        const auto all_data_cells =
+            data_cell::make_all_data_cells(input.get_data_fields());
+        table test_table(input.header_fields_, all_data_cells);
+        println(stderr, "\nheader_fields_: {}\n", test_table.header_fields_);
+        println(stderr, "\ndata_fields: {}\n", test_table.cell_rows_);
+        println(stderr, "\ntype_fieldname_map:\n");
+        for (const auto& v : test_table.type_to_column_name_map) {
+            const auto v_first = str(v.first);
+            const auto v_second = v.second;
+            println(stderr, "\ndata_fields: {}\n", v_first);
+            // const string prefix{"\t{"};
+            // const string rest{"{}, {}} "};
+            // println(stderr, "{}", prefix);
+            // println(stderr, rest, str(v.first), v.second);
+        }
+        println(stderr, "\n");
+        println(stderr, "Index for \"Filename\": {}", test_table.column_name_index_map["Filename"]);
+        println(stderr, "Index for \"Image Size (MB)\": {}", test_table.column_name_index_map["Image Size (MB)"]);
+        // println(stderr, "\nmap: {}\n", test_table.column_name_index_map);
+    }
 }
