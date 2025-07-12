dea35181242de05af8c6f2677fe3e10317518efe
Jim Tubman <jim.tubman@me.com>
2025-07-12 13:18:31 -0600

    Added some basic access functions.

--- table.hpp
+++ table.hpp
@@ -7,6 +7,7 @@
 #include <ranges>
 #include <set>
 #include <string>
+#include <string_view>
 #include <utility>
 #include <vector>
 
@@ -21,9 +22,14 @@ using std::map;
 using std::pair;
 using std::set;
 using std::string;
+using std::string_view;
 using std::vector;
+using namespace std::string_literals;
 namespace ranges = std::ranges;
 namespace views = std::ranges::views;
+
+using std::operator""s;
+using std::operator""sv;
 
 const bool has_enumerate_view =
 #if defined(__cpp_lib_ranges_enumerate)
@@ -39,6 +45,8 @@ struct table {
     parser::header_fields header_fields_{};
 
     using cell_rows = vector<data_cells>;
+
+    using cell_column = vector<data_cell>;
 
     /// @brief vector of rows of cells.
     cell_rows cell_rows_{};
@@ -126,6 +134,74 @@ struct table {
         table tmp{std::move(other)};
         swap(tmp);
         return *this;
+    }
+
+    const auto header_at_index(const size_t idx) const {
+        return header_fields_[idx];
+    }
+
+    const auto column_name_at_index(const size_t idx) const {
+        return header_fields_[idx].name;
+    }
+
+    const auto index_for_column_name(const std::string& col_name) const {
+        return column_name_index_map.at(col_name);
+    }
+
+    constexpr bool is_undetermined(const std::string& col_name) const {
+        const auto idx = index_for_column_name(col_name);
+        return header_at_index(idx).data_type == e_cell_data_type::undetermined;
+    }
+
+    constexpr bool is_invalid(const std::string& col_name) const {
+        const auto idx = index_for_column_name(col_name);
+        return header_at_index(idx).data_type == e_cell_data_type::invalid;
+    }
+
+    constexpr bool is_boolean(const std::string& col_name) const {
+        const auto idx = index_for_column_name(col_name);
+        return header_at_index(idx).data_type == e_cell_data_type::boolean;
+    }
+
+    constexpr bool is_floating(const std::string& col_name) const {
+        const auto idx = index_for_column_name(col_name);
+        return header_at_index(idx).data_type == e_cell_data_type::floating;
+    }
+
+    constexpr bool is_integer(const std::string& col_name) const {
+        const auto idx = index_for_column_name(col_name);
+        return header_at_index(idx).data_type == e_cell_data_type::integer;
+    }
+
+    constexpr bool is_text(const std::string& col_name) const {
+        const auto idx = index_for_column_name(col_name);
+        return header_at_index(idx).data_type == e_cell_data_type::text;
+    }
+
+    constexpr bool is_geo_coordinate(const std::string& col_name) const {
+        const auto idx = index_for_column_name(col_name);
+        return header_at_index(idx).data_type ==
+               e_cell_data_type::geo_coordinate;
+    }
+
+    constexpr bool is_tags(const std::string& col_name) const {
+        const auto idx = index_for_column_name(col_name);
+        return header_at_index(idx).data_type == e_cell_data_type::tags;
+    }
+
+    const data_cells& get_data_row(size_t row_idx) const {
+        return cell_rows_[row_idx];
+    }
+
+    data_cells& get_data_row(size_t row_idx) { return cell_rows_[row_idx]; }
+
+    cell_column get_data_column(size_t column_idx) {
+        return ranges::fold_left(
+            cell_rows_, cell_column{},
+            [&column_idx](cell_column acc, data_cells& dcs) {
+                acc.push_back(dcs[column_idx]);
+                return acc;
+            });
     }
 };
 }  // namespace jt
