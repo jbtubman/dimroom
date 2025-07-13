34c06e72af1def4de23d4072bcf241950bda856f
Jim Tubman <jim.tubman@me.com>
2025-07-13 16:17:43 -0600

    Cleanup of table_test.

--- test/table_test.cpp
+++ test/table_test.cpp
@@ -1,4 +1,5 @@
 #include "../table.hpp"
+#include "../query.hpp"
 
 #include <algorithm>
 #include <filesystem>
@@ -32,30 +33,17 @@ TEST_CASE(MyFixture, Table) {
         const auto all_data_cells =
             data_cell::make_all_data_cells(input.get_data_fields());
         table test_table(input.header_fields_, all_data_cells);
-        // println(stderr, "\nheader_fields_: {}\n", test_table.header_fields_);
-        // println(stderr, "\ndata_fields: {}\n", test_table.cell_rows_);
-        // println(stderr, "\ntype_fieldname_map:\n");
-        for (const auto& v : test_table.column_name_to_type_map) {
-            const auto v_first = v.first;
-            const auto v_second = v.second;
-            // println(stderr, "\ndata_fields: {}\n", v_first);
-            // const string prefix{"\t{"};
-            // const string rest{"{}, {}} "};
-            // println(stderr, "{}", prefix);
-            // println(stderr, rest, str(v.first), v.second);
-        }
-        // println(stderr, "\n");
-        // println(stderr, "Index for \"Filename\": {}", test_table.column_name_index_map["Filename"]);
-        // println(stderr, "Index for \"Image Size (MB)\": {}", test_table.column_name_index_map["Image Size (MB)"]);
+        const string table_json = std::format("{}", test_table);
+        println(stderr, "\ntest_table: {}\n", table_json);
+
+
         auto hd = test_table.header_fields_;
-        //println(stderr,"test_table.header_fields_: {}", hd);
-        // println(stderr, "\nmap: {}\n", test_table.column_name_index_map);
-        //println(stderr, "\ntest_table: {}\n", test_table);
-        auto search_result = string_match(test_table, "Filename", "Iceland.png");
+
+        auto search_result = string_matchx(test_table, "Filename", "Iceland.png");
         CHECK_TRUE(!search_result.empty());
-        search_result = string_match(test_table, "Filename", "Finland.png");
+        search_result = string_matchx(test_table, "Filename", "Finland.png");
         CHECK_TRUE(search_result.empty());
-        search_result = string_match(test_table, "Type", "jpeg");
+        search_result = string_matchx(test_table, "Type", "jpeg");
         CHECK_TRUE(!search_result.empty());
         CHECK_TRUE(search_result.size() == 2);
         println(stderr, "\nsearch for \"Type\" == \"jpeg\": {}\n", search_result);
@@ -64,7 +52,7 @@ TEST_CASE(MyFixture, Table) {
     SECTION("create from table") {
       filesystem::path p{MyFixture::csv_input_file};
       CHECK_TRUE(filesystem::exists(MyFixture::csv_input_file));
-      table test_table = table::make_table_from_file(MyFixture::csv_input_file);
+      jt::table test_table = table::make_table_from_file(MyFixture::csv_input_file);
       // println(stderr, "\ntest_table: \n{}\n", test_table);
       // CHECK_TRUE(test_table.cell_rows_.size() == 5);
     }
