de591a924864eafe8d5e45de5f64b545505e6346
Jim Tubman <jim.tubman@me.com>
2025-07-13 17:53:45 -0600

    Fix to coordinate regex.

--- coordinates.hpp
+++ coordinates.hpp
@@ -27,7 +27,7 @@ const regex deg_min_lat_rx{deg_min_lat_s};
 const string deg_min_lat_starts_s{R"(("(\d{1,2})° (\d{1,2})' ([NS])))"};
 const regex deg_min_lat_starts_rx{deg_min_lat_starts_s};
 
-const string deg_min_long_s(R"(((\d{1,3})° (\d{1,2})' ([EW])))");
+const string deg_min_long_s(R"(((\d{1,3})°s+(\d{1,2})'\s+([EW])))");
 const regex deg_min_long_rx{deg_min_long_s};
 
 const string deg_min_long_end_s(R"(\s\d{1,3}° \d{1,2}' [EW]")");
